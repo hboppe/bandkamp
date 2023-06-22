a016cd57539a82fb55c7472501c50994fd05faec
hboppe <hanna.nascimento31@gmail.com>
2023-06-22 16:50:44 -0400

    refactor: change it to ModelSerializers

--- users/serializers.py
+++ users/serializers.py
@@ -3,30 +3,37 @@ from rest_framework.validators import UniqueValidator
 from .models import User
 
 
-class UserSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+class UserSerializer(serializers.ModelSerializer):
     username = serializers.CharField(
         validators=[
             UniqueValidator(
                 queryset=User.objects.all(),
                 message="A user with that username already exists.",
             )
-        ],
+        ]
     )
+
     email = serializers.EmailField(
-        validators=[UniqueValidator(queryset=User.objects.all())],
+        validators=[
+            UniqueValidator(User.objects.all(), message="This field must be unique.")
+        ]
     )
-    password = serializers.CharField(write_only=True)
-    full_name = serializers.CharField(max_length=50, required=False)
-    artistic_name = serializers.CharField(max_length=50)
 
     def create(self, validated_data: dict) -> User:
         return User.objects.create_user(**validated_data)
 
     def update(self, instance: User, validated_data: dict) -> User:
         for key, value in validated_data.items():
-            setattr(instance, key, value)
+            if key == "password":
+                instance.set_password(value)
+            else:
+                setattr(instance, key, value)
 
         instance.save()
 
         return instance
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "password", "full_name", "artistic_name"]
+        extra_kwargs = {"password": {"write_only": True}}
